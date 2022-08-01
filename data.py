8b0b6e67326e37a3394576d5065aa2596857f089
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-08-01 00:47:21 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,3 @@
 from os import environ
-token: str = environ['token']
+token: str = environ['token'] # (pas dans le code), nous n'allons pas donner le token du bot franchement ^^
 guild_id: int = 1003448652624302221
